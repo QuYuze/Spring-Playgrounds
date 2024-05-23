1aeaa36e6963b241db471d931edcd39b86edff05
Qu Yuze <quyuze09@outlook.com>
2024-05-24 00:00:06 +1000

    First rest controller

--- playground1/src/main/java/com/yuze/springboot/playground1/Playground1Application.java
+++ playground1/src/main/java/com/yuze/springboot/playground1/Playground1Application.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Playground1Application {
 
 	public static void main(String[] args) {
+
+		//bootstrap spring app
 		SpringApplication.run(Playground1Application.class, args);
 	}
 
---
+++ playground1/src/main/java/com/yuze/springboot/playground1/rest/DateController.java
@@ -0,0 +1,16 @@
+package com.yuze.springboot.playground1.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class DateController {
+
+    @GetMapping("/Date")
+    public String getDate(){
+        Date date = new Date();
+        return date.toString();
+    }
+}
