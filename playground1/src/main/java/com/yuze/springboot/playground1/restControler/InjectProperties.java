8806985177aa294167155e56a5c68eac57099b13
Qu Yuze <quyuze09@outlook.com>
2024-05-26 19:08:43 +1000

    -inject properties -add configurations

---
+++ playground1/src/main/java/com/yuze/springboot/playground1/restControler/InjectProperties.java
@@ -0,0 +1,19 @@
+package com.yuze.springboot.playground1.restControler;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InjectProperties {
+
+    //inject property for: author
+    @Value("${author}")
+    private String author;
+
+    @GetMapping("/author")
+    private String getAuthor(){
+        return  "Author: " + author;
+    }
+
+}
