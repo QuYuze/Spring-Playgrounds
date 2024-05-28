19b39c132c4859186fd63ab0074b5166548e3423
Qu Yuze <quyuze09@outlook.com>
2024-05-28 20:39:59 +1000

    hibernate

---
+++ plaground-Hibernate-JPA-CRUD/src/main/java/com/yuze/springboot/plaground_Hibernate_JPA_CRUD/PlagroundHibernateJpaCrudApplication.java
@@ -0,0 +1,13 @@
+package com.yuze.springboot.plaground_Hibernate_JPA_CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlagroundHibernateJpaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlagroundHibernateJpaCrudApplication.class, args);
+	}
+
+}
---
+++ plaground-Hibernate-JPA-CRUD/src/test/java/com/yuze/springboot/plaground_Hibernate_JPA_CRUD/PlagroundHibernateJpaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuze.springboot.plaground_Hibernate_JPA_CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlagroundHibernateJpaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
