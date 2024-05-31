363879b963bbed0ef6e549ec1d4a43f694371ad9
Qu Yuze <quyuze09@outlook.com>
2024-05-31 16:56:42 +1000

    REST-CRUD-APIs

---
+++ playground-REST-CRUD-APIs/src/main/java/com/yuze/springboot/playground_REST_CRUD_APIs/PlaygroundRestCrudApIsApplication.java
@@ -0,0 +1,13 @@
+package com.yuze.springboot.playground_REST_CRUD_APIs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundRestCrudApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaygroundRestCrudApIsApplication.class, args);
+	}
+
+}
---
+++ playground-REST-CRUD-APIs/src/test/java/com/yuze/springboot/playground_REST_CRUD_APIs/PlaygroundRestCrudApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuze.springboot.playground_REST_CRUD_APIs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundRestCrudApIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
